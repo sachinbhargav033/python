acb1316f74c25257d9f7b54cb8b7288bc26a4d64
SACHINBHARGAV <sachinbhargav033@gmail.com>
2026-02-13 21:36:28 -0800

    Add files via upload

---
+++ day-3 operation and condtional statement.py
@@ -0,0 +1,29 @@
+"""#check if number is zero
+num= int(input())
+#
+if num ==0:
+        print("number is zero")
+print("program done") """
+
+
+"""#check if number is zero
+num = int (input())
+
+if num==0
+    print("number is zero")
+else:
+    print("number is not zero")
+print("program done")"""
+
+
+#read two numbers find which one is bigger one
+num1=int(input())
+num2=int(input())
+
+if num1>num2:
+    print("num1 is bigger")
+else:
+    print("num2 is bigger")
+print("program is done")
+
+    
